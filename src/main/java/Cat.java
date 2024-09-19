83e4f9b20152c5394e4de5564a1f3682ac10bf11
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-09-19 13:28:50 +0300

    add task-2.1.2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Cat {
+    private String name;
+    private int age;
+}
